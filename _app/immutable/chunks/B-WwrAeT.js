6aa174628d1483a60c27436c0e5650277b413a51
ghervis <ghervis.megalomaniac@gmail.com>
2025-08-20 12:59:31 +0800

    Updates

---
+++ _app/immutable/chunks/B-WwrAeT.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_24sw72?.base??"/sveltekit-gh-pages",e=globalThis.__sveltekit_24sw72?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/BtBANvxK.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_11nz55h?.base??"/sveltekit-gh-pages",e=globalThis.__sveltekit_11nz55h?.assets??s;export{e as a,s as b};
